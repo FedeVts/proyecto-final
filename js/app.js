8a0df29d4473ae4ff55c6391776e5c9016489184
FedeVts <ramella93@gmail.com>
2022-09-08 23:43:04 -0300

    agregando cosas js

--- js/app.js
+++ js/app.js
@@ -2,26 +2,26 @@ const carrito = [];
 
 const listaProductos = [
   {
-    nombre: "flandria-vainilla",
-    descripcion: "pija dura",
+    nombre: "Flandria Vainilla",
+    descripcion: "Fino tabaco de origen Belga saborizado vainilla",
     precio: 400,
     imagen: "realTABACO FLANDRIA VAINILLA 30gr.jpeg",
   },
   {
-    nombre: "flandria-virginia",
-    descripcion: "pitito",
+    nombre: "Flandria Virginia",
+    descripcion: "Fino tabaco de origen Belga del tipo Virginia Blend",
     precio: 400,
     imagen: "2flandria-silver-tabaco-precios.jpg",
   },
   {
-    nombre: "flandria-chocolate",
-    descripcion: "culo",
+    nombre: "Cerrito Vainilla",
+    descripcion: "Tabaco importado de Chile sabor vainilla",
     precio: 400,
     imagen: "4tabaco-cerrito-vainilla-venta-768x768.jpg.webp",
   },
   {
-    nombre: "flandria-pija",
-    descripcion: "culito",
+    nombre: "Achalay Virginia",
+    descripcion: "Fino tabaco importado de variedad Virginia blend",
     precio: 400,
     imagen: "10achalay-tabaco-virginia.webp",
   },
@@ -38,7 +38,6 @@ function agregarACarrito(e) {
 document.addEventListener("DOMContentLoaded", mounted);
 
 function mounted() {
-
   let contenedor = document.getElementById("productos");
   listaProductos.forEach((producto) => {
     let card = document.createElement("div");
@@ -51,6 +50,8 @@ function mounted() {
     />
     <div class="card-body">
     <p class="card-text">
+    <span class="d-block text-center">${producto.nombre}</span>
+    <br>
     ${producto.descripcion}
     </p>
     <button name="botonCarrito" type="button" class="btn btn-primary btn-lg botonCarrito">
@@ -59,55 +60,44 @@ function mounted() {
     </div>
     
     `;
-    card.className = "card mb-3 col mx-4" ;
+    card.className = "card mb-3 col mx-4";
 
     contenedor.append(card);
-
-    let btn_compra = document.querySelectorAll("[name='botonCarrito']");
-    console.log( btn_compra);
-   
   });
 }
 
-
-
-
-
-
+let btn_compra = document.querySelectorAll("[name='botonCarrito']");
+console.log(btn_compra);
 
 // for( let boton of btn_compra){
-    
+
 //     boton.addEventListener("click" , agregar_a_carrito);
 // }
 
-
 // function agregar_a_carrito(e){
-    
-    //     console.log("EL EVENTO ESTA EN:" , e.target);
-    
-    
-    //     let hijo = e.target;
-    //     let padre = hijo.parentNode;
-    //     let abuelo = padre.parentNode;
-    //     //console.log(padre);
-    //     //console.log(abuelo);
-    
-    
-    //     let nombre_producto = padre.querySelector("h5").textContent;
-    
-    //     let precio = padre.querySelector("span").textContent;
-    //     let img = abuelo.querySelector("img").src;
-    //     //console.log(nombre_producto);
-    //     //console.log(precio);
-    //     //console.log(img);
-    
-    //     let producto = {
-        //         nombre:nombre_producto,
+
+//     console.log("EL EVENTO ESTA EN:" , e.target);
+
+//     let hijo = e.target;
+//     let padre = hijo.parentNode;
+//     let abuelo = padre.parentNode;
+//     //console.log(padre);
+//     //console.log(abuelo);
+
+//     let nombre_producto = padre.querySelector("h5").textContent;
+
+//     let precio = padre.querySelector("span").textContent;
+//     let img = abuelo.querySelector("img").src;
+//     //console.log(nombre_producto);
+//     //console.log(precio);
+//     //console.log(img);
+
+//     let producto = {
+//         nombre:nombre_producto,
 //         img:img,
 //         precio: precio,
 //         cantidad:1
 //     };
-
 
 //     carrito.push(producto);
 
@@ -118,7 +108,5 @@ function mounted() {
 
 //     mostrar_carrito( producto );
 // }
-
-
 
 // botonCarrito.addEventListener("click", agregarACarrito);
