d8dfb1f40de2e54d9db56e4be7373452f52f83bf
FedeVts <ramella93@gmail.com>
2022-09-19 19:00:04 -0300

    puliendo detalles

--- js/app.js
+++ js/app.js
@@ -1,8 +1,4 @@
-
-
-
-let carrito = []
-
+let carrito = [];
 
 const listaProductos = [
   {
@@ -30,24 +26,61 @@ const listaProductos = [
     id: 04,
     nombre: "Achalay Virginia",
     descripcion: "Fino tabaco importado de variedad Virginia blend",
-    precio:750,
+    precio: 750,
     imagen: "10achalay-tabaco-virginia.webp",
   },
+
   {
-    id: 04,
-    nombre: "Achalay Virginia",
-    descripcion: "Fino tabaco importado de variedad Virginia blend",
-    precio:750,
-    imagen: "10achalay-tabaco-virginia.webp",
+    id: 05,
+    nombre: "Saint Natural",
+    descripcion:"tabaco Virginia y tabaco Burley sin aditivos y con el sabor natural al tabaco.",
+    precio: 750,
+    imagen: "5saints-tabaco-natural-precio.jpg",
   },
   {
-    id: 04,
-    nombre: "Achalay Virginia",
+    id: 06,
+    nombre: "Don Jose Dark",
+    descripcion: "Tabaco importado de Mexico variedad Dark black",
+    precio: 750,
+    imagen: "6tabaco-don-jose-precio.jpg",
+  },
+  {
+    id: 07,
+    nombre: "Sairy Natural",
     descripcion: "Fino tabaco importado de variedad Virginia blend",
-    precio:750,
-    imagen: "10achalay-tabaco-virginia.webp",
+    precio: 750,
+    imagen: "7Tabaco-Sayri-tabaqueria-online-pop.webp",
+  },
+  {
+    id: 08,
+    nombre: "Achalay Menta",
+    descripcion: "Fino tabaco importado de variedad Virginia blend",
+    precio: 750,
+    imagen: "9tabaco-achalay-menta-venta.jpg",
+  },
+  {
+    id: 09,
+    nombre: "Sairy Natural",
+    descripcion: "Fino tabaco importado de variedad Virginia blend",
+    precio: 750,
+    imagen: "7Tabaco-Sayri-tabaqueria-online-pop.webp",
+  },
+  {
+    id: 10,
+    nombre: "Sairy Natural",
+    descripcion: "Fino tabaco importado de variedad Virginia blend",
+    precio: 750,
+    imagen: "7Tabaco-Sayri-tabaqueria-online-pop.webp",
+  },
+  {
+    id: 11,
+    nombre: "Sairy Natural",
+    descripcion: "Fino tabaco importado de variedad Virginia blend",
+    precio: 750,
+    imagen: "7Tabaco-Sayri-tabaqueria-online-pop.webp",
   },
   
+
 ];
 
 // const botonCarrito = document.getElementsByClassName("botonCarrito");
@@ -59,39 +92,35 @@ function agregarACarrito(e) {
 }
 
 document.addEventListener("DOMContentLoaded", crearCards);
-window.addEventListener("load", ()=>{
-  if (window.location == "../pages/galeriaProductos"){
-    return
-  }
-  else{
+window.addEventListener("load", () => {
+  if (window.location == "../pages/galeriaProductos") {
+    return;
+  } else {
     Swal.fire({
-      title: 'Sos Mayor de Edad?',
+      title: "Sos Mayor de Edad?",
       text: "Esta pagina es solo para Mayores de 18 años",
-      icon: 'warning',
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonColor: '#d33',
-      cancelButtonColor: '#3085d6',
-      confirmButtonText: 'No soy mayor de edad',
-      cancelButtonText:'Soy mayor de edad'
+      confirmButtonColor: "#d33",
+      cancelButtonColor: "#3085d6",
+      confirmButtonText: "No soy mayor de edad",
+      cancelButtonText: "Soy mayor de edad",
     }).then((result) => {
       if (result.isConfirmed) {
-        Swal.fire(
-          'Prohibido',
-          'No podes ingresar.',
-          'error'
-        )
-        window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley")
+        Swal.fire("Prohibido", "No podes ingresar.", "error");
+        window.location.replace(
+          "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+        );
       }
-    })
+    });
   }
-  
-})
+});
 
 function crearCards() {
   let contenedor = document.getElementById("productos");
   for (let p of listaProductos) {
     let card = document.createElement("div");
-    
+
     card.innerHTML = `
     <img
     src="../assets/img-productos/${p.imagen}"
@@ -104,7 +133,7 @@ function crearCards() {
     <br>
     ${p.descripcion}
     </p>
-    <span class="d-block text-center"><strong>${p.precio}</strong></span>
+    <span class="d-block text-center"><strong> ${p.precio}</strong></span>
     <br>
     <button name="botonCarrito" type="button" class="btn btn-primary btn-lg botonCarrito">
     Agregar a Carrito
@@ -113,125 +142,96 @@ function crearCards() {
     
     `;
     card.className = "card mb-3 col mx-4";
-    
-    let btn_compra = document.getElementsByClassName('botonCarrito');
+
+    let btn_compra = document.getElementsByClassName("botonCarrito");
     console.log(btn_compra);
     contenedor.append(card);
-    for( let boton of btn_compra){
+    for (let boton of btn_compra) {
       console.log(boton);
-    
-      boton.addEventListener("click" , agregarCarrito)
-       
+
+      boton.addEventListener("click", agregarCarrito);
     }
+  }
+}
+
+function agregarCarrito(e) {
+  let marcador = e.target.parentNode;
+  let nombreProducto = marcador.querySelector("h5").textContent;
+  let precioProducto = parseInt(marcador.querySelector("span").textContent);
+  let fotoProducto = marcador.parentNode.querySelector("img").src;
+  let producto = {
+    nombre: nombreProducto,
+    precio: precioProducto,
+    imagen: fotoProducto,
   };
+
+  carrito.push(producto);
+  console.log(carrito);
+
+  renderizarCarrito();
 }
 
-function agregarCarrito(e){
-let marcador = e.target.parentNode;
-let nombreProducto = marcador.querySelector('h5').textContent
-let precioProducto = parseInt(marcador.querySelector('span').textContent)
-let fotoProducto = marcador.parentNode.querySelector('img').src
-let producto = {
-  nombre: nombreProducto,
-  precio: precioProducto,
-  imagen: fotoProducto,
-  
-}
-
-carrito.push(producto)
-console.log(carrito);
-
-renderizarCarrito()
-
-
-}
-
-
-function renderizarCarrito(e){
-  let container = document.getElementById('carrito')
-  let card = document.createElement('div')
-  for(let p of carrito){
-card.classList.add('carrito-container');
+function renderizarCarrito(e) {
+  let container = document.getElementById("carrito");
+  let card = document.createElement("div");
+  for (let p of carrito) {
+    card.classList.add("carrito-container");
     card.innerHTML = `
     <img class="card-img-top" src="${p.imagen}"></img>
     <li>${p.nombre}</li>
-    <li>Precio: $<p class="precio-producto">${p.precio}</p></li>`
+    <li>Precio: $<p class="precio-producto">${p.precio}</p></li>`;
     container.append(card);
     let total = document.getElementById("total");
     let precios = document.getElementsByClassName("precio-producto");
     let sumaTotal = 0;
 
-    for(let precio of precios){
+    for (let precio of precios) {
       sumaTotal += parseInt(precio.innerText);
       total.innerHTML = `${sumaTotal}`;
     }
   }
-  
 }
 
-
-
-const divisa = '$';
-const DOMitems = document.querySelector('#items');
-const DOMcarrito = document.querySelector('#carrito');
-const DOMtotal = document.querySelector('#total');
-let DOMbotonVaciar = document.querySelector('#boton-vaciar');
-let DOMbotonComprar = document.querySelector('#boton-comprar');
+const divisa = "$";
+const DOMitems = document.querySelector("#items");
+const DOMcarrito = document.querySelector("#carrito");
+const DOMtotal = document.querySelector("#total");
+let DOMbotonVaciar = document.querySelector("#boton-vaciar");
+let DOMbotonComprar = document.querySelector("#boton-comprar");
 console.log(DOMbotonVaciar);
 
-let storage = localStorage.setItem('listaProductos', JSON.stringify(carrito))
-
-
-
+let storage = localStorage.setItem("listaProductos", JSON.stringify(carrito));
 
 // renderizarProductos();
 renderizarCarrito();
-DOMbotonVaciar.addEventListener('click', vaciarCarrito);
-DOMbotonComprar.addEventListener('click', comprarProducto ) //no funciona le falta una function que no se que ponerle
+DOMbotonVaciar.addEventListener("click", vaciarCarrito);
+DOMbotonComprar.addEventListener("click", comprarProducto); //no funciona le falta una function que no se que ponerle
 
-
-function vaciarCarrito(){
-carrito = [];
-let container = document.getElementById('carrito')
-container.innerHTML = ""
-let total = document.getElementById("total");
-total.innerHTML = 0;
+function vaciarCarrito() {
+  carrito = [];
+  let container = document.getElementById("carrito");
+  container.innerHTML = "";
+  let total = document.getElementById("total");
+  total.innerHTML = 0;
   renderizarCarrito();
-
-  
 }
 
-function comprarProducto(e){
-
-
+function comprarProducto(e) {
+  Swal.fire({
+    title: "Confirmar compra?",
+    text: "su pedido esta siendo procesado",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Si, confirmar compra",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      vaciarCarrito();
+    } else {
+    }
+  });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //     console.log("EL EVENTO ESTA EN:" , e.target);
 
